6ebd7ecb38b85859cc6f6fa5875153287d0f90d3
Carlos Eduardo ✔ <orlandocarlosjose@gmail.com>
2022-08-08 09:38:48 -0300

    Positivos e Negativos

--- ex02.py
+++ ex02.py
@@ -4,21 +4,25 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 29/06/2022
+# data: 08/08/2022
 #
 # 2 - Escreva uma função que recebe um número n como parâmetro e imprime se n é nulo, positivo ou negativo.
 
-# declarando a função
-def teste(valor1):
-  if valor1 < 0: # verificando se o valor é menor que 0
-    print(f"{valor1} é negativo") # imprimindo que o valor é negativo (valor1)
-  elif valor1 > 0:
-    print(f"{valor1} é positivo") # imprimindo que o valor é positivo (valor1)
-  else: # se o valor for igual a 0
-    print(f"{valor1} é nulo") # imprimindo que o valor é nulo (valor1)
+
+def teste(valor1): # Declarando a função
+  
+  if valor1 < 0: # Verificando se o valor é menor que 0
+    print(f"{valor1} é negativo") # Imprimindo que o valor é negativo (valor1)
+  
+  elif valor1 > 0: # Verificando se o valor é maior que zero
+    print(f"{valor1} é positivo") # Imprimindo que o valor é positivo (valor1)
+  
+  else: # Se o valor for igual a 0
+    print(f"{valor1} é nulo") # Imprimindo que o valor é nulo (valor1)
 
 
-# testando a função com os valores passados pelo usuário
-teste(10) # imprimindo que o valor é positivo (10)
-teste(-10) # imprimindo que o valor é negativo (-10)
-teste(0) # imprimindo que o valor é nulo (0)
+# -- Chamada das funções --
+
+teste(10) # Imprimindo que o valor é positivo (10)
+teste(-10) # Imprimindo que o valor é negativo (-10)
+teste(0) # Imprimindo que o valor é nulo (0)
