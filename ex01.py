0908922c7b93f4e2fa743e246e3a0a64163ad70a
Carlos Eduardo ✔ <orlandocarlosjose@gmail.com>
2022-08-08 09:38:05 -0300

    Maior valor e menor valor

--- ex01.py
+++ ex01.py
@@ -4,21 +4,25 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 29/06/2022
+# data: 08/08/2022
 #
 # 1 - Escreva uma função que recebe dois parâmetros e imprime o menor dos dois.
 # Se eles forem iguais, imprima que eles são iguais.
-#
-def menor(valor1, valor2):  # declarando a função
-    if valor1 < valor2: # verificando se o primeiro valor é menor que o segundo valor
-        print(f"O menor valor é {valor1}") # imprimindo o primeiro valor
-    elif valor1 > valor2: # verificando se o primeiro valor é maior que o segundo valor
-        print(f"O menor valor é {valor2}") # imprimindo o segundo valor
-    else: # se os valores forem iguais
-        print(f"{valor1} e {valor2} são iguais") # imprimindo que os valores são iguais (valor1 e valor2)
+
+def menor(valor1, valor2): # Declarando a função
+    
+    if valor1 < valor2: # Verificando se o primeiro valor é menor que o segundo valor
+        print(f"O menor valor é {valor1}") # Imprimindo o primeiro valor
+    
+    elif valor1 > valor2: # Verificando se o primeiro valor é maior que o segundo valor
+        print(f"O maior valor é {valor2}") # Imprimindo o segundo valor
+    
+    else: # Se os valores forem iguais
+        print(f"{valor1} e {valor2} são iguais") # Imprimindo que os valores são iguais (valor1 e valor2)
 
 
-# testando a função com os valores passados pelo usuário
+# -- Chamada das funções --
+
 menor(10, 20) # imprimindo o menor valor (10)
 menor(20, 10) # imprimindo o menor valor (10)
 menor(10, 10) # imprimindo que os valores são iguais (10 e 10)
